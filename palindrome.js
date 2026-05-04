ff38375c16666f568dd6512e2c0405176f7477b9
logiccrew <syedabdullah009922@gmail.com>
2026-05-04 09:07:20 +0500

    Make a function to Check if a string is a palindrome or not.

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,14 @@
-const palindrome = () => {
-
+const palindrome = (data) => {
+reverse = ""
+    for(  i = data.length - 1 ;i>=0 ; i-- ){
+        reverse += data[i]
+        
+    }
+    console.log(reverse)
+    if(reverse === data){
+        console.log("Your String is palindrome ")
+    }else{
+        console.log("Your String is not palindrome ")
+    }
 }
+palindrome("racecar")
