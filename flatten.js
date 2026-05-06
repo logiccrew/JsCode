92f498f41308c85ba3e213910c77e9fdd27a6e06
logiccrew <syedabdullah009922@gmail.com>
2026-05-06 14:51:09 +0500

    make a Flatten function to faltten the array.

--- flatten.js
+++ flatten.js
@@ -1,1 +1,15 @@
-const flatten = () =>
+const flatten = (data, depth) => {
+
+    result = []
+    for (i = 0; i < data.length; i++) {
+        if (Array.isArray(data[i]) && depth > 0) {
+            result = result.concat(flatten(data[i], depth - 1))
+        } else {
+            result.push(data[i]);
+        }
+    }
+   return result
+
+}
+const arr = [1, [2, [3, [4]]]];
+console.log(flatten(arr, 2))
