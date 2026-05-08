bc2d13707cf0d3a7ebe8ec361e025735b866cb22
logiccrew <syedabdullah009922@gmail.com>
2026-05-08 09:16:06 +0500

    make a function to fetch the data through an end point.

---
+++ getUser.js
@@ -0,0 +1,18 @@
+const getUser = async(id) =>{
+    try {
+        const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    console.log(response)
+
+    if(!response.ok){
+        throw new Error("User not found")
+    }
+    const user = await response.json();
+console.log(user.name)
+    return user.name;
+    } catch (error) {
+        console.log("Something went wrong", error)
+    }
+}
+getUser(1)
